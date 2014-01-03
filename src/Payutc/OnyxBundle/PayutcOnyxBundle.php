1277936fdae225249e161116f1fbafe6a611c583
Matthieu Guffroy <mattgu74@gmail.com>
2014-01-03 15:13:25 +0100

    Configuration du CAS

--- src/Payutc/OnyxBundle/PayutcOnyxBundle.php
+++ src/Payutc/OnyxBundle/PayutcOnyxBundle.php
@@ -2,8 +2,17 @@
 
 namespace Payutc\OnyxBundle;
 
+use Payutc\OnyxBundle\DependencyInjection\Security\Factory\CasFactory;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PayutcOnyxBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new CasFactory());
+    }
 }
