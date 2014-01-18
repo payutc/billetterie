87385f68346ef460519fd7183af0c640a16f607d
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2014-01-18 16:59:55 +0100

    Restore CasListener.php

--- src/Payutc/OnyxBundle/Security/Firewall/CasListener.php
+++ src/Payutc/OnyxBundle/Security/Firewall/CasListener.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Payutc\OnyxBundle\Security\Firewall;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -8,22 +7,17 @@ use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
-
 use Payutc\OnyxBundle\Security\Authentication\Token\CasToken;
 
 class CasListener implements ListenerInterface
 {
     protected $securityContext;
     protected $authenticationManager;
-    protected $casUrl;
-    protected $devBaseUrl;
 
-    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager, $casUrl, $devBaseUrl)
+    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager)
     {
         $this->securityContext = $securityContext;
         $this->authenticationManager = $authenticationManager;
-        $this->casUrl = $casUrl;
-        $this->devBaseUrl = $devBaseUrl;
     }
 
     public function handle(GetResponseEvent $event)
@@ -32,24 +26,6 @@ class CasListener implements ListenerInterface
 
         // Check ticket CAS, if OK analyze it, if not, return;
         $ticket = $request->get('ticket');
-        if ($ticket) {
-            $cas = new Cas($this->casUrl);
-            try {
-                $user = $cas->authenticate($ticket, $this->devBaseUrl);
-            } catch (\Exception $e) {
-                // Deny authentication with a '403 Forbidden' HTTP response
-                $response = new Response();
-                $response->setStatusCode(403);
-                $event->setResponse($response);
-                return;
-            }
-        } else {
-            return;
-        }
-
-        // Check ticket CAS, if OK analyze it, if not, return;
-        $ticket = $request->get('ticket');
-        
         if(!$ticket) {
             return;
         }
@@ -74,8 +50,8 @@ class CasListener implements ListenerInterface
             $this->securityContext->setToken($authToken);
         } catch (AuthenticationException $failed) {
             // To deny the authentication clear the token. This will redirect to the login page.
-            //$this->securityContext->setToken(null);
-            //return;
+            $this->securityContext->setToken(null);
+            return;
         }
     }
 }
